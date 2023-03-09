effe1edc0cfb822bdfe56227b06a8e72eccc9edf
Kirsty Leishman <leishman.kirsty@gmail.com>
2023-03-09 17:18:08 +0000

    created connection string for postgresql db in production

--- src/database/database.ts
+++ src/database/database.ts
@@ -1,4 +1,6 @@
-import { Sequelize } from "sequelize";
-
-// TODO: This should be external config
-export const sequelize = new Sequelize("sqlite::memory:");
+import { Dialect, Sequelize } from "sequelize";
+export let sequelize = new Sequelize("sqlite::memory:");
+if (process.env.NODE_ENV !== 'test') {
+    const connString = `${process.env.DB_DIALECT ?? 'postgres'}://${process.env.DB_USERNAME ?? 'MISSING_DB_USERNAME_CONFIG'}:${process.env.DB_PASSWORD ?? 'MISSING_DB_PASSWORD_CONFIG'}@${process.env.DB_HOST ?? 'MISSING_DB_HOST_CONFIG'}/${process.env.DB_NAME ?? 'MISSING_DB_NAME_CONFIG'}`;
+    sequelize = new Sequelize(connString);
+}
